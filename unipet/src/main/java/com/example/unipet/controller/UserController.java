cea6892a4a788411ced2ff0164dbe098b1bcc2a7
test <139727181+ljs1990-tj@users.noreply.github.com>
2026-04-16 10:22:17 +0900

    new user

--- unipet/src/main/java/com/example/unipet/controller/UserController.java
+++ unipet/src/main/java/com/example/unipet/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.unipet.controller;
 
-import java.util.HashMap;
+import java.util.HashMap; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
