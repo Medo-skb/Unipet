72ca3db898214f98d658065edb5fcb22a15cec00
Medo-skb <kiryto0912@gmail.com>
2026-04-17 10:24:21 +0900

    fix : security 설정 조정

---
+++ unipet/src/main/java/com/example/unipet/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.unipet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+	    return http.build();
+	}
+    
+    @Bean
+    public HttpFirewall allowDoubleSlashHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedDoubleSlash(true); // 더블 슬래시 허용
+        return firewall;
+    }
+
+
+}
