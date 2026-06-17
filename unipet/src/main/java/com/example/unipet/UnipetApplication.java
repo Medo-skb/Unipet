6b92f6d56e96457d779cfd483710d8ca4e6660a1
test <tjrwowkd@gmail.com>
2026-06-17 17:55:39 +0900

    feat: reservation 260617 예약 조회페이지 개

--- unipet/src/main/java/com/example/unipet/UnipetApplication.java
+++ unipet/src/main/java/com/example/unipet/UnipetApplication.java
@@ -2,11 +2,17 @@ package com.example.unipet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@EnableScheduling
-public class UnipetApplication {
+public class UnipetApplication extends SpringBootServletInitializer {
+	// 추가 코드
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UnipetApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnipetApplication.class, args);
